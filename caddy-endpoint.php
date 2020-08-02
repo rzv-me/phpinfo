34abe1847c951516d9dba5409d874ec4b05c055d
Razvan Aurariu <38325118+rzv-me@users.noreply.github.com>
2020-08-02 18:59:46 +0200

    added lb domain to the checkpoint

--- caddy-endpoint.php
+++ caddy-endpoint.php
@@ -2,7 +2,7 @@
     function endsWith( $str, $sub ) {
         return ( substr( $str, strlen( $str ) - strlen( $sub ) ) == $sub );
     }
-    if(!endsWith($_GET['domain'], "gomagcaddy.rzv.me")) {
+    if(!endsWith($_GET['domain'], "gomagcaddy.rzv.me") && !endsWith($_GET['domain'], "gomagcaddylb.rzv.me")) {
         header('HTTP/1.0 403 Forbidden');
         echo 'You are forbidden!';
     }
